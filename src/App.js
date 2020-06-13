4c726a4b7fe1083f52dedc906c92c3b4b73e40d7
Sylvain Maurier <sylvain.maurier@gmail.com>
2020-06-13 23:25:41 +0200

    wip : metronome

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import React, { useState } from 'react';
 import './App.css';
+import sound1 from './sound/beat1.wav';
+import sound2 from './sound/beat2.wav';
 
 function App() {
+
+  const beat1 = new Audio(sound1);
+  const beat2 = new Audio(sound2);
 
   const [playing, setPlaying] = useState(false);
   const [count, setCount] = useState(0);
   const [bpm, setBpm] = useState(100);
   const [beatsPerMeasure, setBeatsPerMeasure] = useState(4);
 
-  this.handleBpmChange = event => {
-    setBpm({ bpm })
-  }
+  let timer = null
+
+  const handleBPM = event => {
+    setBpm(event.target.value);
+  };
+
+  const startStop = () => {
+    if (playing) {
+      // Timer stop
+      clearInterval(timer);
+      setPlaying(false)
+    } else {
+      // Timer Start
+      timer = setInterval(
+        playClick,
+        (60 / bpm) * 1000
+      );
+      setPlaying(true)
+      playClick()
+    }
+  };
+
+  const playClick = () => {
+    // Change le son du premier beat
+    if (count % beatsPerMeasure === 0) {
+      beat2.play();
+    } else {
+      beat1.play();
+    }
+    setCount((count+1) % beatsPerMeasure)
+    console.log("count", count);
+  };
+
 
   return (
     <div className="metronome">
       <div className="bpm-slider">
         <div>{bpm} BPM</div>
-        <input type="range" min="60" max="240" value={bpm} onChange={this.handleBpmChange}/>
+        <input type="range" min="60" max="240" defaultValue={bpm} onMouseUp={handleBPM} />
       </div>
-      <button>{playing ? 'Stop' : 'Start'}</button>
+      <button onClick={startStop}>{playing ? 'Stop' : 'Start'}</button>
     </div>
   );
 }
